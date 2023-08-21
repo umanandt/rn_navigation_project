139ce30d4a8642be067585a6e1e3d2ce9484a504
umanand triapthi <umanandblog@gmail.com>
2023-08-21 15:58:07 +0530

    updated after drawer and Stack Nested navigation - has easy to do adding drawer function ino stack as component

--- App.js
+++ App.js
@@ -4,30 +4,76 @@ import CategoriesScreen from "./screens/CategoriesScreen";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import FavoritesScreen from "./screens/FavoritesScreen";
+import { Ionicons } from "@expo/vector-icons";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealsDetailScreen from "./screens/MealsDetailScreen";
 
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "white",
+        // contentStyle became sceneConatinerStyle  to change styles
+        sceneContainerStyle: {
+          backgroundColor: "#3f2f25",
+        },
+        drawerContentStyle: { backgroundColor: "#351401" },
+        drawerInactiveTintColor: "white",
+        drawerActiveTintColor: "#351101",
+        drawerActiveBackgroundColor: "#e4baa1",
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: "All Catrgories",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen name="Favorites" component={FavoritesScreen}
+      options={{
+          title: "All Favorites",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="star" color={color} size={size} />
+          ),
+        }}
+       />
+    </Drawer.Navigator>
+  );
+}
+
+// function name could be anything and but Drawer.Navigator is important
+// we have defined everything here and use the function name
+// call it in Stack.Screen
+
 export default function App() {
-  const Stack = createNativeStackNavigator();
   return (
     <>
       <GestureHandlerRootView style={styles.container}>
         <StatusBar style="light" />
         <NavigationContainer>
           <Stack.Navigator
-            screenOptions={{
-              headerStyle: { backgroundColor: "#351401" },
-              headerTintColor: "white",
-              contentStyle: {
-                backgroundColor: "#3f2f25",
-              },
-            }}
+          // I can cut and paster screen option (see drawer.navigator) for all the setting
           >
             <Stack.Screen
-              name="MealsCategories"
-              component={CategoriesScreen}
+              name="Drawer"
+              //component={CategoriesScreen}
+              component={DrawerNavigator}
+              // rather than calling a components I called Drawer function and Drawer
+              // function has all the components
+
               options={{
-                title: "All categories",
+                // title: "All categories",
+                headerShown: false,
               }}
             />
             <Stack.Screen
@@ -40,7 +86,6 @@ export default function App() {
               // }
               // }} we can do all here another of setting "OPTIONS"
             />
-
             <Stack.Screen
               name="MealsDetailScreen"
               component={MealsDetailScreen}
@@ -78,3 +123,5 @@ const styles = StyleSheet.create({
 //<Stack.Screen name="ProductDetails" component={ProductDetails} /> // initial screen
 //</Stack.Navigator>
 //
+
+// npm install react-native-safe-area-context@4.6.3 react-native-screens@3.22.0 --save --save-exact
